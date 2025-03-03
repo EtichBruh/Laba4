512be466eee92dbdb9b40f677baa16b85fcd911c
ALEKsANder <alexanderkrumin2008@gmail.com>
2025-03-03 15:16:14 +0300

    Code  completed

--- laba4.py
+++ laba4.py
@@ -1,8 +1,8 @@
-N = int(input())
+N = int(input('введите n'))
 n = range(1,N)
 #(1^5 + 2^5 + 3^5 +…+ n^5) +(1^7 + 2^7 + 3^7+…+ n^7) = 2 (1 + 2 + 3 +…+ n)^4
-summ1 = 1
-summ2 = 1
+summ1 = 0
+summ2 = 0
 for i in n:
   summ1 += i**5
   summ2 += i**7
@@ -11,3 +11,4 @@ print(total)
 
 answer = 2 * sum(n)**4
 print(answer)
+print(total,' = ',answer)
